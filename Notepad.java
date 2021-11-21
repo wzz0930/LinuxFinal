922da8c4356382b64309184b887ed61b7e04e964
wzz <2376220230@qq.com>
2021-11-21 17:16:33 +0800

    my Final test

---
+++ Notepad.java
@@ -0,0 +1,15 @@
+public class Notepad{
+  public String content;
+  public String time;
+  public int id;
+  public void setNotepadContent(String content) {
+    this.content = content;
+}
+  public void setNotepadTime(String time){
+    this.time = time;
+}
+  public void setId(int id){
+    this.id = id;
+}
+
+}
